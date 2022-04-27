2cae903dc257a989b8d5800ae7b95b451d69021b
Zaida Hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-04-27 00:32:53 +0000

    edited code

--- js/script.js
+++ js/script.js
@@ -2,7 +2,26 @@
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
-function myButtonClicked() {
-  // this function displays "Hello, World!"
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>";
+"use strict";
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit5-01-HTML/sw.js", {
+    scope: "/ICS2O-Unit5-01-HTML/",
+  });
 }
+
+/**
+ * This function determines if an integer is negative or positive.
+ */
+function myButtonClicked() {
+  const userInteger = parseInt(document.getElementById('integer').value)
+  if (userInteger > -1) {
+  document.getElementById("answer").innerHTML = "The integer you input is positive."
+  } else {
+  document.getElementById("answer").innerHTML = "The integer you input is a negative."
+}
+}
+
